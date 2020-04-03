1023c2c9b96d1310edc7a9fb43ad8299a2a4e841
Sez Huawei DEV <sez.huawei.dev@gmail.com>
2020-04-03 17:12:04 +0530

    Test folder updated. Build not yet checked.

---
+++ src/test/java/test/app/infy/util/BusServiceHelperTest.java
@@ -0,0 +1,14 @@
+package test.app.infy.util;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BusServiceHelperTest {
+
+	@Test
+	public void contextLoads() {
+		//works.
+	}
+	
+}
